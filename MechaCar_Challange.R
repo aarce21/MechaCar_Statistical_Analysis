b90c3b95c566b96792ab11405daa41fc0ddf49b6
Angela Arce <101824443+aarce21@users.noreply.github.com>
2022-07-03 15:54:01 -0400

    Deliverable 1, 2, and 3 completed

--- MechaCar_Challange.R
+++ MechaCar_Challange.R
@@ -1,4 +1,5 @@
 library(dplyr)
+# Deliverable 1:
 mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) #read in the csv file as a df
 head(mechacar_mpg_df)
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df) #perform linear regression
